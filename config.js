da8f93e94e7e9f6227b5c82ba1bb70d949f9c675
codef80 <codef3y@gmail.com>
2026-07-06 18:00:26 +0300

    Add window.APP_CONFIG with Supabase settings

--- config.js
+++ config.js
@@ -4,3 +4,54 @@ export const APP_CONFIG = {
   SUPABASE_ANON_KEY: "sb_publishable_tMeu-SpwNjndnfatONiyqw_3GUW6qfM",
   FUNCTIONS_URL: "https://ghuiotbjwefwwguvzphe.supabase.co/functions/v1",
 };
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://ghuiotbjwefwwguvzphe.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_tMeu-SpwNjndnfatONiyqw_3GUW6qfM",
+ 
+  // مسار دوال Edge (عادة نفس المشروع)
+  FUNCTIONS_URL: "https://ghuiotbjwefwwguvzphe.supabase.co/functions/v1",
+};
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
